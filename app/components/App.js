f0aaaae7ccd87ecd8908ab1916469371c4d5e162
ryuken <ryuken73@gmail.com>
2020-11-02 17:51:06 +0900

    etc : making HLSPlayer.js(convert functional component), but there is something wrong to handle redirected m3u8 response in video.js

--- app/components/App.js
+++ app/components/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import FullHeightContainer from './template/FullHeightContainer';
 import FirstChildSection from './template/FirstChildSection';
-import VideoPlayer from './VideoPlayer';
+import HLSPlayer from './HLSPlayer';
 import Html5Player from './Html5Player';
 import WebView from './WebView';
 
@@ -21,11 +21,16 @@ const theme = createMuiTheme({
   },
 });
 const players = [1]
-
+// video.js can't follow 302 redirect. see below.
+// * incorrect url is made based on initial url not redirected url
+// correct : https://cctvsec.ktict.co.kr:8082/livekbs/9965/playlist.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9MTAvMjgvMjAyMCAzOjA0OjU0IFBNJmhhc2hfdmFsdWU9RXZnMGxLbk83QStabmt3aTF3TzRQUT09JnZhbGlkbWludXRlcz0xMjA=
+// incorrect : https://cctvsec.ktict.co.kr/9965/e9kLhEFmUD4LN5nutFjuZHnD9JrGKrFt75U6ttodXKVg8OTT6ti+Mhl7lQnZZywM2h56Ksu/chunks.m3u8?nimblesessionid=66101573&wmsAuthSign=c2VydmVyX3RpbWU9MTEvMi8yMDIwIDY6NTU6MTEgQU0maGFzaF92YWx1ZT02VFFvZUIwa3FqbFgza2tpZ0pOYTFRPT0mdmFsaWRtaW51dGVzPTEyMA==
+const src = "https://cctvsec.ktict.co.kr/9965/e9kLhEFmUD4LN5nutFjuZHnD9JrGKrFt75U6ttodXKVg8OTT6ti+Mhl7lQnZZywM2h56Ksu/xP9wUIQeftwdEA=="
+// const src = "d:/temp/cctv/stream.m3u8";
 function App() {
   const videoPlayers = []
   for(let i=0;i<players.length;i++){
-    videoPlayers.push(<VideoPlayer key={players[i]} controls={true} autoplay={true} bigPlayButton={true}></VideoPlayer>);
+    videoPlayers.push(<HLSPlayer src={src} key={players[i]} controls={true} autoplay={true} bigPlayButton={true}></HLSPlayer>);
     console.log(i)
   }
   console.log(videoPlayers)
